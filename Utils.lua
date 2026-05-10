bcac8f83264188a306e99a4d5e6007e65ace50f2
Pows <ksi.seam24@gmail.com>
2026-05-10 10:20:49 +0700

    Update Utils.lua

--- Utils.lua
+++ Utils.lua
@@ -1,10 +1,11 @@
+-- Utils.lua
 local Repository = "https://raw.githubusercontent.com/Yenixs/ToolScript"
-local CascadeLibrary = loadstring(game:HttpGet(Repository.. "/refs/heads/main/Cascade.luau"))();
+local CascadeLibrary = loadstring(game:HttpGet(Repository.. "/refs/heads/main/Cascade.luau"))()
 local GuiMinimizeButton = loadstring(game:HttpGet(Repository.. '/refs/heads/main/GuiToggle.luau'))()
 local ConfigManager = loadstring(game:HttpGet(Repository.. "/refs/heads/main/ConfigManager.luau"))()
 
 return {
-  UILibrary = CascadeLibrary,
-  MimizeButton = GuiMinimizeButton,
-  ConfigManager = ConfigManager,
+    UILibrary = CascadeLibrary,
+    MinimizeButton = GuiMinimizeButton,
+    ConfigManager = ConfigManager,
 }
